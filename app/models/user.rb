c61e53455ed3a1a725923a6543127e0bf58bc66e
mikael autheman <mika@atoomik.fr>
2020-01-08 17:45:28 +0100

    fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  attr_accessor :login
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_hash).where("lower(username) = :value OR lower(email) = :value", value: login.downcase).first
-    else
-      where(conditions.to_hash).first
-    end
-
-  end
 
 end
