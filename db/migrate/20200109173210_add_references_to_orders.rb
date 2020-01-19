d6ab644eebf5c5da64599dd5b287c7b04823695a
mikael autheman <mika@atoomik.fr>
2020-01-19 22:38:43 +0100

    add theme to orders

--- db/migrate/20200109173210_add_references_to_orders.rb
+++ db/migrate/20200109173210_add_references_to_orders.rb
@@ -2,5 +2,6 @@ class AddReferencesToOrders < ActiveRecord::Migration[5.2]
   def change
       add_reference :orders, :user, foreign_key: true
       add_column :orders, :name, :string, null: false, default: ""
+      add_column :orders, :theme, :text
   end
 end
